637524ef36dd3c901c4f535b1b875de72e162dbc
CarolinaViana <carolina.coast@hotmail.com>
2016-05-06 16:38:19 -0300

    Arquivo PHP

---
+++ aula0605/index.php
@@ -0,0 +1,41 @@
+<?php 
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta charset="UTF-8">
+
+<title>Formulário</title>
+
+</head>
+
+<body>
+
+ 
+  <form action="Formulario.php" method="post">
+
+
+
+	Login: <br />
+
+	<input type="text" name="Login" required="required"> <br /><br />
+
+        Senha: <br />
+
+	<input type="password" name="senha" required="required"> <br><br />
+          
+        Enviar
+	<input type = "submit" value = "Logar"required="required" >
+
+
+</form>
+
+
+
+</body>
+
+</html>
+?>
